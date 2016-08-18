2b5fee893ef2d747ed3b423ef3f0d01e815d3f81
Xyronpl <xyron987@gmail.com>
2016-08-18 12:43:51 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -39,31 +39,6 @@ public class Main {
 		System.out.printf("Suma kwot to: %.2fPLN\n", suma);
 	}
 	
-	public double liczSume(File file) throws FileNotFoundException
-	{
-		int a, b;
-		double suma = 0;
-		String linia;
-		String kwota;
-		double dKwota;
-		File f = file;
 
-		Scanner sc = new Scanner(f);
-
-		do {
-			linia = sc.nextLine();
-			a = linia.indexOf("@amount:");
-			b = linia.indexOf("PLN");
-			if (a != -1) {
-				a += 8;
-				kwota = linia.substring(a, b);
-				kwota = kwota.replace(",", ".");
-				dKwota = Double.parseDouble(kwota);
-				suma += dKwota;
-			}
-		} while (sc.hasNextLine());
-		
-		return suma;
-	}
 
 }
